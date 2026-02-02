a50d2ee8c9ba5f24a0b26167b00539ecbab8d8fb
drannnf <drflores2020@plm.edu.ph>
2026-02-02 15:25:17 +0800

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import Header from './components/Header'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Header />
+  </StrictMode>
+)
