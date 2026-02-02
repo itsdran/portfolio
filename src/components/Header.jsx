99a8ff3eccb47fb7e87af98ef684521373629f91
drannnf <drflores2020@plm.edu.ph>
2026-02-02 19:36:07 +0800

    Intial design for the Profile section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+
+import '../header.css';
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <div className="header">
+            <div className="name-initials">Dranoel Flores</div>
+            <div className="nav-links">
+                <ul className="nav-list">
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#about">About Me</a></li>
+                    <li><a href="#projects">Projects</a></li>
+                    <li><a href="#contacts">Contact</a></li>
+                    <li><a href="#resume">Resume</a></li>
+                </ul>
+            </div>
+        </div>
     )
 }
 
---
+++ src/components/Profile.jsx
@@ -0,0 +1,19 @@
+const Profile = () => {
+    return (
+        <div className="profile">
+            <div className="info">
+                <div className="profile-name">Dranoel Flores</div>
+                <div className="profile-title">Full Stack Developer</div>
+                <div className="profile-desc">
+                    Hi! I'm Dran, a learning Full Stack Developer with a passion for creating dynamic and responsive web applications.
+                </div>
+            </div>
+
+            <div className="profile-picture">
+                <img src="src/assets/profile-pic.png" alt="Profile Picture" />
+            </div>
+        </div>
+    )
+}
+
+export default Profile
